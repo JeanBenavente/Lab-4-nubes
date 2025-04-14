9ed93ad71d7a673d784bbb0bbc25f9ff776f42df
iJeann1 <jeanpierobenaventerojas@gmail.com>
2025-04-13 19:28:32 -0500

    Modificación de los .js

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const app = express();
+const port = 9000;
 
-const PORT = 3000;
+// Importar rutas
+const clientesRoutes = require('./routes/clientes');
+const productosRoutes = require('./routes/productos');
 
-app.get('/', (req, res) => {
-  res.send('¡Bienvenido a la página principal!');
-});
+// Usar rutas
+app.use('/clientes', clientesRoutes);
+app.use('/productos', productosRoutes);
 
-app.get('/about', (req, res) => {
-  res.send('Esta es la página "Acerca de".');
-});
-
-app.get('/usuario/:nombre', (req, res) => {
-  const nombre = req.params.nombre;
-  res.send(`Hola, ${nombre}!`);
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
